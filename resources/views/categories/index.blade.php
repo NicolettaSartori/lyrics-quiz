0ac4453d91e9b20d9945a1db361bf79365784d9a
Nicoletta2003-1 <71252015+Nicoletta2003-1@users.noreply.github.com>
2021-01-06 14:27:22 +0100

    edited hover effect

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     @if(!$is_super)
         <div class="d-flex justify-content-center">
-            <a href="{{route('category.create', $super_category->id)}}" class="create card text-center shadow mb-4 w-75 bd-acc">
+            <a href="{{route('category.create', $super_category->id)}}"
+               class="gr-acc-hv card text-center shadow mb-4 w-75 bd-acc">
                 <div class="p-0 m-4">
                     <strong class="h4 txt-acc-dark-i">Create new</strong>
                 </div>
@@ -14,17 +15,19 @@
     @foreach($categories as $category)
         <div class="d-flex justify-content-center">
             @if($is_super)
-                <a href="{{route('category', $category->id)}}" class="category card text-center mb-4 w-75">
+                <a href="{{route('category', $category->id)}}" class="gr-main-hv card text-center shadow mb-4 w-75">
                     @else
                         <a href="{{route('category.show', [
                         'super_category' => $super_category->id,
                         'category' => $category->id])}}"
-                           class="category card text-center mb-4 w-75 shadow">
+                           class="gr-main-hv card text-center mb-4 w-75 shadow">
                             @endif
                             <div class="p-0 m-4-tb">
                                 <img src="{{$category->path}}" class="rounded w-50 mx-auto d-block m-1-b" alt="">
                                 <strong class="h4 h4-normal m-2">{{$category->body}}</strong><br>
+                                @if(!$is_super)
                                 <div class="txt-grey-i">Questions: {{$category->questions->count()}}</div>
+                                @endif
                             </div>
                         </a>
         </div>
--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -4,14 +4,14 @@
     @foreach($questions as $question)
         <div class="d-flex justify-content-center">
             <div class="card shadow text-center mb-4 w-75 p-4">
-                <div class="category card text-center mb-2">
+                <div class="card text-center mb-2 gr-acc">
                     <div class=" p-0 m-5">
                         <strong class="h4 h4-normal">{{$question->body}}</strong><br>
                     </div>
                 </div>
 
                 @foreach($question->answers as $answer)
-                    <div class="category card text-center mb-2">
+                    <div class="gr-main-hv category card text-center mb-2">
                         <div class=" p-0 m-4">
                             <strong class="h4 h4-normal">{{$answer->body}}</strong><br>
                         </div>
