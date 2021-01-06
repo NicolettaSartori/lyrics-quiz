a921322107846c4e016565fb5ee3d989e69868eb
Nicoletta2003-1 <71252015+Nicoletta2003-1@users.noreply.github.com>
2021-01-06 11:48:15 +0100

    added question&answer seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Answer;
 use App\Models\Category;
+use App\Models\Question;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -16,5 +18,10 @@ class CategorySeeder extends Seeder
     {
         Category::factory(5)->create(['super_category' => null]);
         Category::factory(5)->create();
+        Question::factory(5)->create();
+        for ($i = 1; $i < 6; $i++) {
+            Answer::factory(3)->create(['question_id' => $i]);
+            Answer::factory()->create(['question_id' => $i, 'is_true' => true]);
+        }
     }
 }
